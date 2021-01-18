dac8694808373f5b80f6f27336cfdfd5be516213
akrawczyk <agammkrawczyk@gmail.com>
2021-01-18 09:35:20 +0100

    first commit

---
+++ src/main/java/mockito/mock/StringManipulator.java
@@ -0,0 +1,12 @@
+package mockito.mock;
+
+public class StringManipulator {
+    public String manipulate(String username) {
+        String input = username.substring(0, 3);
+        if (input.toLowerCase().contains("a")) {
+            return username.toLowerCase();
+        } else {
+            return username.toUpperCase();
+        }
+    }
+}
