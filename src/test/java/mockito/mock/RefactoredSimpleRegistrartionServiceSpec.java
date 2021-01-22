8f67788dc1ae152755a4fd783b0e85e376121c4e
akrawczyk <agammkrawczyk@gmail.com>
2021-01-22 20:02:59 +0100

    init mocks

--- src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
+++ src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
@@ -1,17 +1,31 @@
 package mockito.mock;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
 
 class RefactoredSimpleRegistrartionServiceSpec {
+
+    private UserRepository userRepository = Mockito.mock(UserRepository.class);
+    private BannedUsersClient bannedUsersClient = Mockito.mock(BannedUsersClient.class);
+    private RegistrationService registrationService = new RegistrationService(userRepository, bannedUsersClient);
+    private UserRepository realUserRepository = new JpaUserRepository();
+    private SimpleRegistrationService service = new SimpleRegistrationService();
 
     @Test
     void shouldStoreNewUser() {
 
-        RefactoredSimpleRegistrartionService service= new RefactoredSimpleRegistrartionService(new FakeUserRepository());
+        System.out.println(service.toString());
+        System.out.println(service.getClass());
 
-        User actualUser = service.registerUser("tom");
+        System.out.println(realUserRepository.toString());
+        System.out.println(realUserRepository.getClass());
+
+        System.out.println(userRepository.toString());
+        System.out.println(userRepository.getClass());
+
+        System.out.println(bannedUsersClient.toString());
+        System.out.println(bannedUsersClient.getClass());
+
 
     }
 }
