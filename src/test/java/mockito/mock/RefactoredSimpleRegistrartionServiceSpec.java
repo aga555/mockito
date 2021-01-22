641148ff2622316dc580ec4cd4806a5ca01b2b75
akrawczyk <agammkrawczyk@gmail.com>
2021-01-22 21:02:35 +0100

    Default Behavior Of Mocks

--- src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
+++ src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
@@ -1,30 +1,32 @@
 package mockito.mock;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+
+@ExtendWith(MockitoExtension.class)
 class RefactoredSimpleRegistrartionServiceSpec {
-
-    private UserRepository userRepository = Mockito.mock(UserRepository.class);
-    private BannedUsersClient bannedUsersClient = Mockito.mock(BannedUsersClient.class);
-    private RegistrationService registrationService = new RegistrationService(userRepository, bannedUsersClient);
-    private UserRepository realUserRepository = new JpaUserRepository();
-    private SimpleRegistrationService service = new SimpleRegistrationService();
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private BannedUsersClient bannedUsersClient;
+    @InjectMocks
+    private RegistrationService registrationService;
 
     @Test
-    void shouldStoreNewUser() {
+    void defaultBehaviour() {
 
-        System.out.println(service.toString());
-        System.out.println(service.getClass());
 
-        System.out.println(realUserRepository.toString());
-        System.out.println(realUserRepository.getClass());
+        System.out.println(userRepository.findByUsername("moris"));
+        System.out.println(userRepository.save(new User()));
 
-        System.out.println(userRepository.toString());
-        System.out.println(userRepository.getClass());
-
-        System.out.println(bannedUsersClient.toString());
-        System.out.println(bannedUsersClient.getClass());
+        System.out.println(bannedUsersClient.amountOfBannedAccounts());
+        System.out.println(bannedUsersClient.banRate());
+        System.out.println(bannedUsersClient.isBanned("mike", new Address()));
+        System.out.println(bannedUsersClient.bannedUserId());
 
 
     }
