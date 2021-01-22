9a28fc80ed368a97531f74565a50f8422e42e1eb
akrawczyk <agammkrawczyk@gmail.com>
2021-01-22 21:12:53 +0100

    Stubbing The First Method Call

--- src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
+++ src/test/java/mockito/mock/RefactoredSimpleRegistrartionServiceSpec.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class RefactoredSimpleRegistrartionServiceSpec {
     @Mock
     private UserRepository userRepository;
@@ -18,8 +22,7 @@ class RefactoredSimpleRegistrartionServiceSpec {
 
     @Test
     void defaultBehaviour() {
-
-
+        //object
         System.out.println(userRepository.findByUsername("moris"));
         System.out.println(userRepository.save(new User()));
 
@@ -27,6 +30,14 @@ class RefactoredSimpleRegistrartionServiceSpec {
         System.out.println(bannedUsersClient.banRate());
         System.out.println(bannedUsersClient.isBanned("mike", new Address()));
         System.out.println(bannedUsersClient.bannedUserId());
+    }
+
+    @Test
+    void basicStubbing() {
+
+        Mockito.when(bannedUsersClient.isBanned("mike", new Address())).thenReturn(true);
+
+        System.out.println(bannedUsersClient.isBanned("mike", new Address()));
 
 
     }
